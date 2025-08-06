cbf6c2dbf3da1882a8b98045b700284b624e18a2
vuhoanggiap <120465721+vuhoanggiap@users.noreply.github.com>
2025-08-06 11:57:38 +0700

    Add files via upload

---
+++ bai6VuHoangGiap.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    float a;
+    printf("Hay nhap thu nhap cua ban (Trieu VND): ");
+    scanf("%f", &a);
+
+	float ThuNhapChiuThue = a - 11;
+	float ThuNhapChiuThue1 = a - 18;
+	float ThuNhapChiuThue2 = a - 32;
+	float ThuNhapChiuThue3 = a - 52;
+	float ThuNhapChiuThue4 = a - 80;
+
+    if (a <= 11) {
+        printf("Ban khong phai tra thue");
+    }else if(a <= 18){
+		printf("so tien thue ban phai nop la :%.3f",ThuNhapChiuThue*0.15);
+    }else if(a <= 32){
+    	printf("so tien thue ban phai nop la :%.3f",ThuNhapChiuThue1*0.2 + 7*0.15);
+	}else if(a <= 52){
+		printf("so tien thue ban phai nop la :%.3f",ThuNhapChiuThue2*0.25 + 14*0.2 +7*0.15);
+	}else if(a <= 80){
+		printf("so tien thue ban phai nop la :%.3f",ThuNhapChiuThue3*0.3 + 20*0.25 + 14*0.2 + 7*0.15);
+	}else{
+		printf("so tien thue ban phai nop la :%.3f",ThuNhapChiuThue4*0.35 + 28*0.3 + 20*0.25 + 14*0.2 + 7*0.15);
+	};
+    return 0;
+}
